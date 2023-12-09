024f88b6b105044f839908ea711ae24dce5e8abb
David Gadling <dave@toasterwaffles.com>
2023-12-09 18:23:24 -0500

    Day 3: part 2 is done!

--- src/day3.rs
+++ src/day3.rs
@@ -3,14 +3,17 @@ use std::fmt;
 use std::io::BufRead;
 
 #[derive(Debug, Default)]
-struct Position {
+struct Symbol {
     x: u16,
     y: u16,
+    val: char,
 }
 
 #[derive(Debug, Default)]
 struct PartNumber {
     row: u16,
+    start_col: u16,
+    end_col: u16,
     number: u16,
     left_bound: u16,
     right_bound: u16,
@@ -18,14 +21,26 @@ struct PartNumber {
     lower_bound: u16,
 }
 
-impl fmt::Display for Position {
+impl fmt::Display for Symbol {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+        write!(f, "({:>3}, {:>3})", self.x, self.y)
+    }
+}
+
+impl Symbol {
+    fn get_gear_ratio(self: &Symbol, nums: &Vec<PartNumber>) -> u128 {
+        let gears: Vec<&PartNumber> = nums.iter().filter(|n| n.is_adjacent_to(self)).collect();
+
+        if gears.len() != 2 {
+            return 0;
+        }
+
+        gears.first().unwrap().number as u128 * gears.last().unwrap().number as u128
     }
 }
 
 impl PartNumber {
-    fn is_adjacent_to(self: &PartNumber, pos: &Position) -> bool {
+    fn is_adjacent_to(self: &PartNumber, pos: &Symbol) -> bool {
         // Adjacent means that all of these must be true:
 
         (pos.x >= self.left_bound && pos.x <= self.right_bound)
@@ -37,18 +52,18 @@ impl fmt::Display for PartNumber {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{:03} @ ({:03}-{:03}, {:03}-{:03})",
-            self.number, self.left_bound, self.right_bound, self.top_bound, self.lower_bound
+            "{:>3} @ ({:>3}, {:>3})",
+            self.number, self.start_col, self.row
         )
     }
 }
 
-fn find_numbers_and_symbols() -> (Vec<PartNumber>, Vec<Position>) {
+fn find_numbers_and_symbols() -> (Vec<PartNumber>, Vec<Symbol>) {
     let reader = utils::get_reader_for_day(3);
     let mut curr_num = String::new();
     let mut in_num = false;
     let mut nums: Vec<PartNumber> = Vec::new();
-    let mut syms: Vec<Position> = Vec::new();
+    let mut syms: Vec<Symbol> = Vec::new();
     let mut curr_part = PartNumber::default();
 
     let input_data: Vec<Vec<char>> = reader
@@ -64,6 +79,7 @@ fn find_numbers_and_symbols() -> (Vec<PartNumber>, Vec<Position>) {
                 if !in_num {
                     in_num = true;
                     curr_part.row = line_no as u16;
+                    curr_part.start_col = col_no as u16;
                     curr_part.lower_bound = if line_no == 0 {
                         0 as u16
                     } else {
@@ -82,6 +98,7 @@ fn find_numbers_and_symbols() -> (Vec<PartNumber>, Vec<Position>) {
             // It's either a symbol or a '.'. Either way if we were in the middle of a number, we're not now!
             if in_num {
                 curr_part.right_bound = col_no as u16;
+                curr_part.end_col = (col_no - 1) as u16;
                 curr_part.number = curr_num.parse::<u16>().unwrap();
                 nums.push(curr_part);
 
@@ -94,9 +111,10 @@ fn find_numbers_and_symbols() -> (Vec<PartNumber>, Vec<Position>) {
                 continue;
             }
 
-            syms.push(Position {
+            syms.push(Symbol {
                 x: col_no as u16,
                 y: line_no as u16,
+                val: our_char.clone(),
             });
         }
 
@@ -115,6 +133,19 @@ fn find_numbers_and_symbols() -> (Vec<PartNumber>, Vec<Position>) {
     (nums, syms)
 }
 
+fn day3_part2() {
+    let mut our_sum: u128 = 0;
+
+    let (nums, syms) = find_numbers_and_symbols();
+
+    for sym in syms.iter().filter(|s| s.val == '*') {
+        our_sum += sym.get_gear_ratio(&nums);
+    }
+
+    println!("Day 3, part 2 = {}", our_sum);
+    // 81296995
+}
+
 fn day3_part1() {
     let mut our_sum: u128 = 0;
 
@@ -130,4 +161,5 @@ fn day3_part1() {
 
 pub fn day3() {
     day3_part1();
+    day3_part2();
 }
