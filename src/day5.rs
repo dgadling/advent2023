5b1aef3d280ff7e9745647e1bb8b84bc1c730110
David Gadling <dave@toasterwaffles.com>
2023-12-18 12:09:49 -0500

    Day 5: Part 2 done after getting some hints from the internet 😰

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,5 @@
 use crate::utils;
-use std::io::BufRead;
+use std::{io::BufRead, ops::Range};
 
 #[derive(Debug)]
 struct SeedMapRow {
@@ -72,12 +72,9 @@ fn get_maps() -> Vec<Vec<SeedMapRow>> {
     all_maps
 }
 
-fn day5_part1() {
-    let mut seeds = get_seeds();
-
-    let maps = get_maps();
+fn map_seeds(seeds: &mut Vec<i128>, maps: &Vec<Vec<SeedMapRow>>) {
     for map in maps.iter() {
-        for seed in &mut seeds {
+        for seed in &mut *seeds {
             // First check to see if the source is min/max out-of-bounds and thus doesn't change
             if *seed < map.first().unwrap().source_start || *seed > map.last().unwrap().source_end {
                 continue;
@@ -92,10 +89,71 @@ fn day5_part1() {
             }
         }
     }
+}
+
+fn get_ranges() -> Vec<Range<i128>> {
+    let mut ranges: Vec<Range<i128>> = get_seeds().chunks(2).map(|c| c[0]..c[0] + c[1]).collect();
+
+    ranges.sort_by(|a, b| a.start.cmp(&b.start));
+
+    ranges
+}
+
+fn day5_part1() {
+    let mut seeds = get_seeds();
+    let maps = get_maps();
+
+    map_seeds(&mut seeds, &maps);
+
     println!("Lowest location = {:?}", seeds.iter().min().unwrap());
+}
+
+fn day5_part2() {
+    let maps = get_maps();
+    let mut ranges = get_ranges();
+    let mut range: Range<i128>;
+
+    for map in maps {
+        let mut new_ranges: Vec<Range<i128>> = Vec::new();
+        'next_range: while !ranges.is_empty() {
+            range = ranges.pop().unwrap();
+            for row in &map {
+                let overlap_start = std::cmp::max(range.start, row.source_start);
+                let overlap_end = std::cmp::min(range.end, row.source_end);
+
+                if overlap_start < overlap_end {
+                    // We have some overlap, so let's map what we need to and
+                    // toss anything that's left over back into the ranges Vec
+                    // to try processing again
+                    new_ranges.push((overlap_start + row.delta)..(overlap_end + row.delta));
+
+                    if range.start < overlap_start {
+                        // Everything in the seed range before this mapping row gets pushed back in for further processing
+                        ranges.push(range.start..overlap_start);
+                    }
+
+                    if range.end > overlap_end {
+                        // Everything in the seed range after this mapping row gets pushed back in for further processing
+                        ranges.push(overlap_end..range.end);
+                    }
+                    
+                    continue 'next_range;
+                }
+            }
+            // None of the mapping rows applied to this seed range, toss it in the new pile
+            new_ranges.push(range.start..range.end);
+            continue;
+        }
+
+        ranges = new_ranges;
+    }
+
+    ranges.sort_by(|a,b| a.start.cmp(&b.start));
+
+    println!("Lowest location = {:?}", ranges.first().unwrap().start);
 }
 
 pub fn day5() {
     day5_part1();
-    // day5_part2();
+    day5_part2();
 }
