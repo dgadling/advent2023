410f822749083f71efeff429af6835ac531a2dec
David Gadling <dave@toasterwaffles.com>
2023-12-19 13:46:13 -0500

    utils: Make it easier to get test data

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,16 @@
 use std::{env::current_dir, fs::File, io::BufReader};
 
 pub fn get_reader_for_day(day: u8) -> BufReader<File> {
+    get_reader(format!("day-{}.txt", day))
+}
+
+pub fn get_test_reader_for_day(day: u8) -> BufReader<File> {
+    get_reader(format!("day-{}-test.txt", day))
+}
+
+fn get_reader(file_name: String) -> BufReader<File> {
     let current_dir = current_dir().expect("Can't get current directory?!");
-    let in_f_path = current_dir.join("input").join(format!("day-{}.txt", day));
+    let in_f_path = current_dir.join("input").join(file_name);
     let file = File::open(in_f_path.to_str().unwrap())
         .expect(format!("Really, the path ({:?}) is wrong?", in_f_path).as_str());
 
